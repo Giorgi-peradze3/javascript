5f057d9ea0e7e301859970cbfbc2489325baf38f
Giorgi_peradze3 <giorgiperadze33@gmail.com>
2026-03-09 11:50:45 +0400

    Add files via upload

---
+++ first lesson/script.js
@@ -0,0 +1,66 @@
+// console.log("hello js") 
+// x = 98
+// y = 9.8
+// s = 'european univesity'
+// m = [20, 30, 12, 15, 69]
+// console.log(x, y, s, m)
+// document.writeln("<h1>hello js</h1>")
+// document.writeln("<p>x = +x+</p>")
+// document.writeln(`<p> my fovourite uni is - ${x} </p>`)
+
+// function f1(){
+//     a1 = 23
+//     a2 = 13
+//     a3 = 9
+
+//     return a1
+// }
+
+// console.log(f1())
+
+// document.writeln("<h1>hello js</h1>")
+
+
+function f2(){
+    x = 12
+    y = 13
+    c = x + y
+
+     return c
+}
+
+
+function pract1_1(text){
+    document.writeln(`<div class = "pr1-1">${text}</div>`)
+}
+
+pract1_1("european uni")
+
+function  pract1_3(t, s){
+    document.writeln(`<div style = "font-size:${s}px">${text}</div>`)
+}
+
+
+pract1_3("hello dear students",22)
+
+function pract1_4(x){
+    document.writeln(`<table style = "width:${x};"><li><ul></ul></li><li><ul></ul></li></table>`)
+
+}
+
+pract1_4(200)
+
+function pract1_6(x, y){
+    document.writeln(`<table class="pr1-6" style="height:${x}px; weight:${y}px;"><ul><li></li><li></li></ul><ul><li></li><li></li></ul></table>`)
+}
+
+pract1_6(150,150)
+
+
+
+
+
+
+
+
+
