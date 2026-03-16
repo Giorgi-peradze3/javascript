ea73f1d0e1d656fd38ad356d49d9514ad2e2f94e
European Student <student@eu.local>
2026-03-16 11:42:58 +0400

    classwork2

--- classwork2/script.js
+++ classwork2/script.js
@@ -25,3 +25,79 @@
 // console.log(r*10) // [0,10)
 
 
+//task 1
+// function f1(x){
+//     return Math.ceil(x);
+// }
+
+// console.log(f1(12.76))
+
+
+//task 2
+// function f2(x){
+//     return Math.round(x);
+// }
+// console.log(f2(13.26))
+
+//task 3
+//  function f3(x){
+//      return Math.round(x);
+// }
+//  console.log(f3(12.5))
+
+
+//task 4
+// function f4(x,y){
+//     if(y>x){
+//         return Math.ceil(x)
+//     }else(x < y){
+//         return Math.round(x)
+//     }
+
+
+// }
+
+// console.log(f4(x,y))
+
+
+//task 5
+ function f5(){
+    //return Math.random(Math.random() * 2);
+    t = Math.random()
+    if(t < 0.1){
+        r = 1
+    }else{
+        r = Math.random()
+    }
+ }
+
+ console.log(f5(r))
+
+//task 6
+// function f6(){
+//     return Math.floor(Math.random() * 50) + 5;
+//  }
+
+//  console.log(f6())
+ 
+
+//task 7
+// function f6(x,y){
+//     return Math.floor(Math.random() * x) + y;
+// }
+
+// console.log(f6(x,y))
+
+//task 8
+
+//  function f8(x,y){
+    
+//         return Math.floor(Math.random() * x) + y;
+    
+    
+//  }
+
+//  console.log(f7(x,y))
+
+
+
