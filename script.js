9a23296c667fea16455f1fc03bb5e2e396b66c1a
European Student <student@eu.local>
2026-03-16 10:36:26 +0400

    classwork2

---
+++ script.js
@@ -0,0 +1,27 @@
+// x = 23
+// y = 2.6
+// console.log(x,y)
+
+// t = "45"
+// console.log(t)
+
+// console.log(x+t)
+
+// t1 = '9'
+// t2 = '8'
+// console.log(t1 + t2)
+// console.log(t1 * t2)
+// console.log(t1 - t2)
+// console.log(t1 / t2)
+
+// s1 = 'EU'
+// s2 = 'UNI'
+// console.log(s1 + s2)
+// console.log(s1*4)
+// console.log(s1 - s2)
+
+// r = Math.random() // [0,1)
+// console.log(r)
+// console.log(r*10) // [0,10)
+
+
