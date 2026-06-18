b7b641fa8d6d8211134e63f38a1695aa2f27a642
Giorgi_peradze3 <giorgiperadze33@gmail.com>
2026-06-18 14:20:31 +0400

    Add files via upload

---
+++ lecture12/script.js
@@ -0,0 +1,63 @@
+var count = 0;
+
+// function GenerateNumbers(e){
+
+//     // const container = document.getElementsByClassName("container")[0];
+//     // const textarea = document.getElementsByClassName("textarea")[0];
+//     // const Insert = document.getElementsByClassName("Insert")[0];
+//     // var Amount = 0;
+
+   
+//     //     if(textarea == 'a'){
+//     //         Amount++;
+
+//     //         console.log(Amount)
+            
+//     //     }
+        
+        
+    
+
+//     // Insert.innerHTML = Amount;
+
+//     // w3 help
+    
+// //    const k = document.getElementById("k");
+
+    
+// //     k.addEventListener("keydown", function (event) {
+    
+// //     var amout = event.key;
+// //     var all = 0;
+// //     if(amout === 'a')
+// //     all++;
+// //     document.getElementById("demo").innerHTML = "You pressed: " + all;
+
+// //     });
+
+//     // var result = document.getElementById("result")
+//     // if(e.key == 'a'){
+//     //     count++
+//     //     var div = document.createElement("div")
+//     //     div.innerHTML = `a- ${count}`
+//     //     result.appendChild(div)
+//     // }
+
+
+// }
+
+var censore = ['cheating', 'lazy', 'irresponsible']
+function HideCensore(e){
+
+    var textarea = document.getElementById("textarea")
+   
+
+
+
+}
+
+
+
+
+
+
